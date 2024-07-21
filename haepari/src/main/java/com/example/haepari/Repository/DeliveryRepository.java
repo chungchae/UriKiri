1af739d25c7b4320cc54cb7d463df89017e61d07
chungchae0 <chungchae0@gmail.com>
2024-07-22 03:36:23 +0900

    Repository

---
+++ haepari/src/main/java/com/example/haepari/Repository/DeliveryRepository.java
@@ -0,0 +1,16 @@
+package com.example.haepari.Repository;
+
+import com.example.haepari.Entity.Delivery;
+import com.example.haepari.Entity.Request;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DeliveryRepository extends JpaRepository<Delivery, Integer> {
+    List<Delivery> findAllByRequest(Request request);
+    @Transactional
+    Delivery save(Delivery delivery);
+}
--- haepari/src/main/java/com/example/haepari/Repository/RequestRepository.java
+++ haepari/src/main/java/com/example/haepari/Repository/RequestRepository.java
@@ -1,6 +1,7 @@
 package com.example.haepari.Repository;
 
 import com.example.haepari.Entity.Request;
+import com.example.haepari.Entity.User;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,8 @@ import java.util.List;
 @Repository
 public interface RequestRepository extends JpaRepository<Request,Integer> {
     List<Request> findAllByDestination_address(String destination_address);
-
+    List<Request> findAllByUser(User user);
+    Request findById(int requestId);
     @Transactional
     Request save(Request request);
 }
