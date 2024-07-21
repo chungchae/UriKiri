b35fc711fa0519c3cac252cc2e8613f0f0c53999
KimMinHwan <127469367+rlaalsghks8@users.noreply.github.com>
2024-07-21 19:05:49 +0900

    "프로젝트 생성 및 공용 DB 생성"

---
+++ haepari/src/main/java/com/example/haepari/Data/Entity/User.java
@@ -0,0 +1,7 @@
+package com.example.haepari.Data.Entity;
+
+
+public class User {
+    private String name;
+
+}
---
+++ haepari/src/main/java/com/example/haepari/HaepariApplication.java
@@ -0,0 +1,13 @@
+package com.example.haepari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaepariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaepariApplication.class, args);
+    }
+
+}
