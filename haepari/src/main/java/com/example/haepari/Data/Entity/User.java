d8432970d363b0dc3606d2affc870eb4e412f89c
KimMinHwan <127469367+rlaalsghks8@users.noreply.github.com>
2024-07-21 19:24:58 +0900

    Feat:"User Entity 생성"

--- haepari/src/main/java/com/example/haepari/Data/Entity/User.java
+++ haepari/src/main/java/com/example/haepari/Data/Entity/User.java
@@ -1,7 +1,26 @@
 package com.example.haepari.Data.Entity;
 
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-    private String name;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String userId;
+    private String id;
+    private String password;
+    private String nickName;
+
+    private float star;
+    private int point;
+
 
 }
