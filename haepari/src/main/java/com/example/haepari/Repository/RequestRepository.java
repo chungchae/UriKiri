85476a5bba88ca9d19470c4242682f824882d920
chungchae0 <chungchae0@gmail.com>
2024-07-22 03:08:09 +0900

    RequestRepository

---
+++ haepari/src/main/java/com/example/haepari/Repository/RequestRepository.java
@@ -0,0 +1,16 @@
+package com.example.haepari.Repository;
+
+import com.example.haepari.Entity.Request;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RequestRepository extends JpaRepository<Request,Integer> {
+    List<Request> findAllByDestination_address(String destination_address);
+
+    @Transactional
+    Request save(Request request);
+}
